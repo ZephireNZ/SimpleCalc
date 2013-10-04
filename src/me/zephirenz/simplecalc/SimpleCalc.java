4edea517009b164110203ea3d77002b161c4ea86
Brynley McDonald <brynleymcdonald@gmail.com>
2013-10-04 13:51:42 +1300

    Start of project

---
+++ src/me/zephirenz/simplecalc/SimpleCalc.java
@@ -0,0 +1,20 @@
+package me.zephirenz.simplecalc;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleCalc implements Runnable, ActionListener {
+
+    public void run() {
+
+    }
+
+    public void main(String[] args) {
+
+    }
+
+    public void actionPerformed(ActionEvent event) {
+
+    }
+
+}
